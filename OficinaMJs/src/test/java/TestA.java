18cadaaf9dbb2a6d42024c5fd53bb79b67acf18a
AntonioB360 <antoniodossantosmenesesmorais@gmail.com>
2026-04-25 13:53:38 +0100

    base do projecto criada

---
+++ OficinaMJs/src/test/java/TestA.java
@@ -0,0 +1,28 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author T
+ */
+public class TestA {
+    
+    @Test
+    void teste(){
+        
+        int r=6+4;
+        
+       assertEquals(10,r);
+        
+        
+        
+    }
+    
+}
